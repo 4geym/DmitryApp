53286b2c80052298ae9a715e5dbd8abcc05a9d63
Dmitry <mr.4geym123@gmail.com>
2021-12-07 01:11:48 +0200

    Final app (Maybe)

---
+++ app/src/main/java/com/example/dmitryapp/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.dmitryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    int quantity = 0;
+    Spinner spinner;
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
+
+    HashMap goodsMap;
+    String goodsName;
+    double price;
+
+    EditText userNameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userNameEditText = findViewById(R.id.nameEditText);
+
+        createSpinner();
+
+        createMap();
+    }
+
+    void createSpinner() {
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        spinnerArrayList = new ArrayList();
+
+        spinnerArrayList.add("Cheese burger");
+        spinnerArrayList.add("Seafood");
+        spinnerArrayList.add("Coffee");
+        spinnerArrayList.add("Another");
+
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+    }
+
+    void createMap() {
+        goodsMap = new HashMap();
+        goodsMap.put("Cheese burger", 3.0);
+        goodsMap.put("Seafood", 10.0);
+        goodsMap.put("Coffee", 1.0);
+        goodsMap.put("Another", 0.0);
+    }
+
+    public void increaseQuantity(View view) {
+        quantity = quantity + 1;
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
+        quantityTextView.setText("" + quantity);
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+    }
+
+    public void decreaseQuantity(View view) {
+        quantity = quantity - 1;
+        if (quantity < 0) {
+            quantity = 0;
+        }
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
+        quantityTextView.setText("" + quantity);
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        goodsName = spinner.getSelectedItem().toString();
+        price = (double)goodsMap.get(goodsName);
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+
+        ImageView goodsImageView = findViewById(R.id.goodsImageView);
+
+        switch (goodsName) {
+            case "Cheese burger":
+                goodsImageView.setImageResource(R.drawable.cheese);
+                break;
+            case "Seafood":
+                goodsImageView.setImageResource(R.drawable.seafood);
+                break;
+            case "Coffee":
+                goodsImageView.setImageResource(R.drawable.coffe);
+                break;
+            case "Another":
+                goodsImageView.setImageResource(R.drawable.asd);
+                break;
+            default:
+                goodsImageView.setImageResource(R.drawable.error);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void addToCart(View view) {
+        Order order = new Order();
+
+        order.userName = userNameEditText.getText().toString();
+        order.goodsName = goodsName;
+        order.quantity = quantity;
+        order.price = price;
+        order.orderPrice = quantity * price;
+
+        Intent orderIntent = new Intent(MainActivity.this, OrderActivity.class);
+        orderIntent.putExtra("userNameForIntent", order.userName);
+        orderIntent.putExtra("goodsName", order.goodsName);
+        orderIntent.putExtra("quantity", order.quantity);
+        orderIntent.putExtra("price", order.price);
+        orderIntent.putExtra("orderPrice", order.orderPrice);
+        startActivity(orderIntent);
+    }
+}
---
+++ app/src/main/java/com/example/dmitryapp/Order.java
@@ -0,0 +1,11 @@
+package com.example.dmitryapp;
+
+public class Order {
+    String userName;
+    String goodsName;
+    int quantity;
+    double price;
+    double orderPrice;
+
+
+}
